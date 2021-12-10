e6110c09c58ef2512acded3630994118c087c950
[valyn dsilva] <[valyndsilva@gmail.com]>
2021-12-10 11:20:45 +0000

    update coding challenges

--- JS_Coding_Challenges/app.js
+++ JS_Coding_Challenges/app.js
@@ -920,14 +920,48 @@ document.querySelector('body').addEventListener('click', function(){
 // A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 // Your tasks:
 // Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
-// 1. Juliafoundoutthattheownersofthefirstandthelasttwodogsactuallyhave cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
-// 2. CreateanarraywithbothJulia's(corrected)andKate'sdata
-// 3. Foreachremainingdog,logtotheconsolewhetherit'sanadult("Dognumber1
+// 1. Julia found out that the owners of the first dog and the last two dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+// 2. Create an array with both Julia's(corrected)and Kate's data
+// 3. For each remaining dog,log to the console whether it's an adult ("Dog number 1
 // is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy   ")
-// 4. Runthefunctionforbothtestdatasets
+// 4. Run the function for both test data sets
 // Test data:
-// § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3] § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+// § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3] 
+// § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 // Hints: Use tools from all lectures in this section so far 😉
+
+const checkDogs = function(dogsJulia, dogsKate){
+  
+  // 1.
+  const dogsJuliaCorrected = dogsJulia.slice(); // [3, 5, 2, 12, 7]
+  dogsJuliaCorrected.splice(0,1); // [3]
+  dogsJuliaCorrected.splice(-2); // [12, 7]
+  console.log(dogsJuliaCorrected); // [5,2]
+
+  // Alternative method using slice
+  // const dogsJuliaCorrected = dogsJulia.slice(1,3);
+ 
+  // 2.
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+ // Alternate method to concat using ...spread operator
+  // const dogs = [...dogsJuliaCorrected, ...dogsKate];
+  // console.log(dogs);
+
+  // 3.
+  dogs.forEach(function(dog, index){
+    if(dog >= 3){
+      console.log(`Dog number ${index+1} is an adult, and is ${dog} years old`)
+    } else {
+      console.log(`Dog number ${index+1} is still a puppy`)
+    }
+  });    
+}
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// 4. 
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 // Coding Challenge #2
 // Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
